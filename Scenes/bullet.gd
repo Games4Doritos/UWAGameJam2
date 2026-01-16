56ee355f476123f1a38f36e5110e0ee276884930
Joshua Noble <joshuarachpaul@hotmail.com>
2026-01-16 22:28:05 +0800

    feat: Enemy added
    # New features:
    - Added enemy model.
    - Added ragdoll for enemy model.
    - If bullet collides with enemy it ragdolls.
    
    # Todo
    - Have enemy actually shoot the player.
    - Fix ragdolls if there is time.

--- Scenes/bullet.gd
+++ Scenes/bullet.gd
@@ -24,3 +24,10 @@ func _process(delta: float) -> void:
 
 func _on_timer_timeout() -> void:
 	queue_free()
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	mesh.visible = false
+	particles.emitting = true
+	await get_tree().create_timer(1.0).timeout
+	queue_free()
---
+++ Scenes/enemy.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+		if area.is_in_group("bullet"):
+			$RootNode/Skeleton3D/PhysicalBoneSimulator3D.physical_bones_start_simulation()
