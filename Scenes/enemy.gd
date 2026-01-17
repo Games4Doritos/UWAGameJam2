f995ae95785a5e396093efbec30ae78a463385e6
Joshua Noble <joshuarachpaul@hotmail.com>
2026-01-17 11:26:56 +0800

    feat: Enemy Projectile
    Added:
    - Enemy projectile
    - Projectile follows the player.
    
    Need to add:
    - game end if projectile touches player.
    - Shooting enemy projectiles to delete them.

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+@onready var timer = $Timer
+
+var ball = preload("res://Scenes/electric_ball.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,3 +16,10 @@ func _process(delta: float) -> void:
 func _on_area_3d_area_entered(area: Area3D) -> void:
 		if area.is_in_group("bullet"):
 			$RootNode/Skeleton3D/PhysicalBoneSimulator3D.physical_bones_start_simulation()
+
+
+func _on_timer_timeout() -> void:
+	var ballInstance = ball.instantiate()
+	ballInstance.position = global_position
+	
+	get_tree().current_scene.add_child(ballInstance)
---
+++ electric_ball.gd
@@ -0,0 +1,19 @@
+extends Area3D
+
+var player
+
+const SPEED = 4
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player = get_tree().get_nodes_in_group("player")[0]
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += (player.position - position).normalized() * delta * SPEED
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		print("ded")
