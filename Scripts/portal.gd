74a1c38f0b41181bee51ed90a9a954f925854342
Joshua Noble <joshuarachpaul@hotmail.com>
2026-01-17 21:37:35 +0800

    feat: win screen
    - Added Camera to portal.
    - Added win screen UI.
    - Prevent player from dying after win.

--- Scripts/portal.gd
+++ Scripts/portal.gd
@@ -3,4 +3,8 @@ extends Node3D
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
+		get_tree().current_scene.gameover = true
+		$Camera3D.make_current()
+		$Win.show()
+		
 		print("Victory")
