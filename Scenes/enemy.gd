36790a843dc46928b9ddf52ad26408baa4e7282e
Joshua Noble <joshuarachpaul@hotmail.com>
2026-01-17 14:08:50 +0800

    fix: Invisible enemy
    # Problem
    Once the enemy has been killed you still can't walk through it.
    
    #Solution
    Deleting the static body inside the enemy once they were killed.

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -16,6 +16,7 @@ func _process(delta: float) -> void:
 func _on_area_3d_area_entered(area: Area3D) -> void:
 		if area.is_in_group("bullet"):
 			$RootNode/Skeleton3D/PhysicalBoneSimulator3D.physical_bones_start_simulation()
+			$StaticBody3D.queue_free()
 			$Timer.stop()
 			$AudioStreamPlayer3D.stop()
 
