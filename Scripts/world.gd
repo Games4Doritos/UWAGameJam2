6e5596c27d03fbddf4d51d13e3eeadd1d7f3dc4d
Joshua Noble <joshuarachpaul@hotmail.com>
2026-01-17 18:59:37 +0800

    fix: Gravity applied to Ragdolls
    # Problem
    When gravity changed the ragdolls still fell in the default gravity direction.
    
    # Solution
    I had to update the gravity scale of each bone in each enemy to be the negative of what it was before (applying gravity in the opposite direction).

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -11,3 +11,8 @@ func gameOver() -> void:
 
 func _on_gravity_timer_timeout() -> void:
 	player.flip_gravity()
+	
+	for enemy in get_tree().get_nodes_in_group("enemy"):
+		for bone in enemy.get_node("RootNode/Skeleton3D/PhysicalBoneSimulator3D").get_children():
+			if bone is PhysicalBone3D:
+				bone.gravity_scale = -bone.gravity_scale
